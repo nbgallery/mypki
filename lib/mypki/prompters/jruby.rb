990a6b2bae94300de4d1732c3a13ab9c31a639d1
Kyle King <kylejking@gmail.com>
2016-05-15 22:28:04 -0600

    fixed jruby support

--- lib/mypki/prompters/jruby.rb
+++ lib/mypki/prompters/jruby.rb
@@ -1,12 +1,12 @@
-require 'mypki/prompters/readline'
-require 'mypki/jruby/jline-2.12.1.jar'
+require 'mypki/prompters/cli'
+require 'mypki/jruby/jline-2.14.1.jar'
 java_import 'jline.console.ConsoleReader'
 
 module MyPKI
   module Prompter
-    class JRubyPrompter < ReadlinePrompter
+    class JRubyPrompter < CommandLinePrompter
       def pass_prompt prompt
-        ConsoleReader.new.read_line prompt, ' '.ord
+        ConsoleReader.new.read_line(prompt, ' '.ord)
       end
     end
   end
